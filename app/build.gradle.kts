24b3440aaf962fdfe4898786322f7649b9fee94d
Diego Matus <dmatus15@alumnos.utalca.cl>
2025-05-05 20:40:19 -0400

    Defining the modules on app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,19 +41,54 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.coil.compose)
+
+    // Compose
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
+
+    // Core
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Crypto
+    implementation(libs.androidx.security.crypto.ktx)
+
+   
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Location
+
+    // Splash screen
+    implementation(libs.androidx.core.splashscreen)
+
+    implementation(projects.core.database)
+    implementation(projects.core.data)
+    implementation(projects.core.domain)
+    implementation(projects.core.presentation.ui)
+    implementation(projects.core.presentation.designsystem)
+
+    implementation(projects.feature.meditation.data)
+    implementation(projects.feature.meditation.domain)
+    implementation(projects.feature.meditation.presentation)
+
+    implementation(projects.feature.auth.data)
+    implementation(projects.feature.auth.domain)
+    implementation(projects.feature.auth.presentation)
+
+
+
 }
